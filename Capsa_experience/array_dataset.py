bb4c0e22d4316ab337f174e932043183fed9e5f7
Samer Attrah <70598009+Samir-atra@users.noreply.github.com>
2023-05-04 09:46:12 +0000

    add array_dataset.py to capsa_experience

---
+++ Capsa_experience/array_dataset.py
@@ -0,0 +1,36 @@
+# dataset
+
+import numpy as np
+import os
+import cv2
+
+
+def dataset(data_path):
+   
+    img_data_array=[]
+    class_name=[]
+
+
+   
+    for dir1 in os.listdir(data_path):
+        for file in os.listdir(os.path.join(data_path, dir1)):
+            image_path= os.path.join(data_path, dir1,  file)
+            image= cv2.imread( image_path, cv2.COLOR_BGR2RGB)
+            image=cv2.resize(image, (64, 64))
+            image=np.array(image)
+            image = image.astype('float32')
+            image /= 255 
+            if len(image.shape) < 3:
+                continue
+            elif dir1 == "meningioma_tumor":
+                img_data_array.append(image)
+                class_name.append(1)
+            elif dir1 =="no_tumor":
+                img_data_array.append(image)
+                class_name.append(0)
+
+    img_data_array = np.stack(img_data_array, axis=0)
+    class_name = np.stack(class_name, axis=0)
+
+
+    return img_data_array, class_name
